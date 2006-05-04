66e9c803e304ae42a2283926df27f17f16f06c98
Rob Thomas <xrobau@gmail.com>
2006-05-04 09:57:29 +0000

    Whoops, forgot to add the new install file

---
+++ install.php
@@ -0,0 +1,30 @@
+<?php
+
+global $db;
+
+// Manage upgrade from DISA 1.0
+// r2.0 Add Timeouts and add wait for confirmation
+$sql = "SELECT digittimeout FROM disa";
+$check = $db->getRow($sql, DB_FETCHMODE_ASSOC);
+if(DB::IsError($check)) {
+	// add new fields - Digit Timeout
+	$sql = 'ALTER TABLE disa ADD COLUMN digittimeout INT DEFAULT "5"';
+	$result = $db->query($sql);
+	if(DB::IsError($result)) {
+		die($result->getDebugInfo());
+	}
+	// Response Timeout
+	$sql = 'ALTER TABLE disa ADD COLUMN resptimeout INT DEFAULT "10"';
+	$result = $db->query($sql);
+	if(DB::IsError($result)) {
+		die($result->getDebugInfo());
+	}
+	$sql = 'ALTER TABLE disa ADD COLUMN needconf VARCHAR ( 10 )  DEFAULT ""';
+	$result = $db->query($sql);
+	if(DB::IsError($result)) {
+		die($result->getDebugInfo());
+	}
+}
+
+
+?>
