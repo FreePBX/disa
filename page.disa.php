c7425e5283dfae6c422ffa8f917b212c5372a890
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-28 14:53:41 -0500

    update license

--- page.disa.php
+++ page.disa.php
@@ -2,7 +2,6 @@
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
 //	Copyright 2013 Schmooze Com Inc.
-//  Copyright (C) 2006 Rob Thomas <xrobau@gmail.com>
 //
 $tabindex = 0;
 $action = isset($_REQUEST['action'])?$_REQUEST['action']:'';
