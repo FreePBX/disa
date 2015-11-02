916369393e267d3274bdf530a6c9d6b73eedb0a5
James Finstrom <jfinstrom@sangoma.com>
2015-11-02 11:04:48 -0700

    menu adjustments

--- views/bootnav.php
+++ views/bootnav.php
@@ -5,15 +5,22 @@ $dataurl = "ajax.php?module=disa&command=getJSON&jdata=grid";
   <a href='?display=disa&view=form' class="btn btn-default"><i class="fa fa-plus"></i> <?php echo _("Add DISA")?></a>
   <a href='?display=disa' class="btn btn-default"><i class="fa fa-list"></i> <?php echo _("DISA List")?></a>
 </div>
-<table id="disagridrnav" data-url="<?php echo $dataurl?>" data-cache="false" data-toolbar="#toolbar-rnav" data-toggle="table" data-pagination="true" data-search="true" class="table table-striped">
+<table id="disagridrnav"
+      data-url="<?php echo $dataurl?>"
+      data-cache="false"
+      data-toolbar="#toolbar-rnav"
+      data-toggle="table"
+      data-search="true"
+      class="table">
   <thead>
     <tr>
-      <th data-field="disa_id" data-formatter="disarnavFormatter" class="col-md-2"><?php echo _("DISA")?></th>
+      <th data-field="displayname" class="col-md-2"><?php echo _("DISA")?></th>
     </tr>
   </thead>
 </table>
+
 <script type="text/javascript">
-function disarnavFormatter(value, row, index){
-  return '<a href="?display=disa&view=form&itemid='+value+'">'+row['displayname']+'</a>';
-}
+	$("#disagridrnav").on('click-row.bs.table',function(e,row,elem){
+		window.location = '?display=disa&view=form&itemid='+row['disa_id'];
+	})
 </script>
