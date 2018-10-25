08dd9099e3945082f6d0e277aa81209e7b4d991b
James Finstrom <jfinstrom@sangoma.com>
2018-10-25 20:23:23 +0000

    Add Legacy support for Backup 15.0

--- Restore.php
+++ Restore.php
@@ -2,11 +2,27 @@
 namespace FreePBX\modules\Disa;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    foreach ($configs as $item) {
-        $id = $item['disa_id'];
-        $this->FreePBX->Disa->edit($id, $item);
-    }
-  }
+	public function runRestore($jobid){
+		$configs = reset($this->getConfigs());
+		foreach ($configs as $item) {
+				$id = $item['disa_id'];
+				$this->FreePBX->Disa->edit($id, $item);
+		}
+		return $this;
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
+		$tables = array_flip($tables + $unknownTables);
+		if (!isset($tables['disa'])) {
+			return $this;
+		}
+		$disa = $this->FreePBX->Disa;
+		$disa->setDatabase($pdo);
+		$configs = $disa->listAll();
+		$disa->resetDatabase();
+		foreach ($configs as $item) {
+			$id = $item['disa_id'];
+			$disa->edit($id, $item);
+		}
+		return $this;
+	}
 }
